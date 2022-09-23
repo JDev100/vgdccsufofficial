760a4ad15c7f9fb241309035581e64e94a35b1ce
JDev100 <95455992+JDev100@users.noreply.github.com>
2022-09-23 16:16:16 -0700

    Next goal, finish landing and work on about section

---
+++ components/sections/skillsrow.js
@@ -0,0 +1,10 @@
+// import React from 'react'
+// import {Flex, Heading, Stack, Link, Image, Text } from "@chakra-ui/react";
+
+// const SkillsRow = () => {
+//   return (
+//     <div>SkillsRow</div>
+//   )
+// }
+
+// export default SkillsRow
