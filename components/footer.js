3635434af211119d60c1aa7de5df8cf95c271f46
JDev100 <95455992+JDev100@users.noreply.github.com>
2022-09-23 16:10:23 -0700

    Added Link to Repository to Footer

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,19 @@
-import { Box } from '@chakra-ui/react'
-
+import { Box, Flex, Link, IconButton } from '@chakra-ui/react'
+import NextLink from "next/link"
+import {AiFillGithub}  from 'react-icons/ai'
 const Footer = () => {
   return (
-    <Box align="center"  fontSize="sm" mt={5}>
+    <Box align="center"  fontSize="sm" mt={5} px={8}>
+      <Flex justify='space-between'>
       &copy; {new Date().getFullYear()} Jesus Cazares. All Rights Reserved.
+      <NextLink  href='https://github.com/JDev100/vgdccsufofficial/' target='_blank'>
+        <IconButton
+        size='lg'
+        fontSize='25px'
+        icon={<AiFillGithub/>}
+        />
+      </NextLink>
+      </Flex>
     </Box>
   )
 }
